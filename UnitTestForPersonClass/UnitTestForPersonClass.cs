1cf92d7bd795fe9baf908791afab1e95430a4728
jimmymcperson <undeadwalrus@hotmail.com>
2016-06-01 17:06:35 -0400

    Added Unit Test project

---
+++ UnitTestForPersonClass/UnitTestForPersonClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForLesson3
+{
+    [TestClass]
+    public class UnitTestForPersonClass
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ UnitTestForPersonClass/UnitTestForProgramClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson3;
+
+namespace UnitTestForLesson3
+{
+    [TestClass]
+    public class UnitTestForProgramClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
