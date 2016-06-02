82278dac5f7abb627624f10d73768e139fd723c0
jimmymcperson <undeadwalrus@hotmail.com>
2016-06-02 16:45:48 -0400

    Added methods and constructors and instantiated objects.

--- COMP123-S2016-Lesson3/Program.cs
+++ COMP123-S2016-Lesson3/Program.cs
@@ -5,14 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lesson3
-{
+    {
     /**
     * This class is the "driver" class for our Program
     *
     * @class Program
     */
     public class Program
-    {
+        {
         /**
         * The main method for our driver class Program
         *
@@ -20,19 +20,35 @@ namespace COMP123_S2016_Lesson3
         * @param {string[]} args
         */
         public static void Main(string[] args)
-        {
+            {
             // create a new instance of the Person class
-            Person person = new Person("tom");
+            Person tanya = new Person();
+            tanya.Name = "Tanya";
+            tanya.Age = 28;
+            tanya.SaysHello();
+            tanya.ShowAge();
 
-            Console.WriteLine(person.Name); // shows the constructor triggering
+            Console.WriteLine();
 
-            // set a value in the Name property of the Person class
-            person.Name = "Tom";
+            Person tom = new Person("Tom");
+            tom.Age = 47;
+            tom.SaysHello();
+            tom.ShowAge();
 
-            // get the Name value and print to console
-            Console.WriteLine(person.Name);
+            Console.WriteLine();
 
-            
+            Person mark = new Person(30);
+            mark.Name = "Mark";
+            mark.SaysHello();
+            mark.ShowAge();
+
+            Console.WriteLine();
+
+            Person mary = new Person("Mary", 23);
+            mary.SaysHello();
+            mary.ShowAge();
+
+
+            }
         }
     }
-}
