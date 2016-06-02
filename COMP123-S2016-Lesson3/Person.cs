585dc66b72dd9b59b1640bc23825aaa79d53492e
jimmymcperson <undeadwalrus@hotmail.com>
2016-06-01 22:49:39 -0400

    Updated Person class to include Name property and two constructors.

--- COMP123-S2016-Lesson3/Person.cs
+++ COMP123-S2016-Lesson3/Person.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lesson3
 {
+    /**
+    * This is class defines a generic Person
+    *
+    * @class Person
+    * @field _name {string}
+    */
     public class Person
     {
+        // PRIVATE INSTANCE VARIABLES =========================================================
+        private string _name;
+
+        // PUBLIC PROPERTIES ==================================================================
+
+            /**
+            * This is a property for our _name field
+            *
+            * @propert {string} Name
+            */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        
+        }
+
+        /**
+        * This is the default/empty constructor for the Person class
+        *
+        * @constructor Person
+        */
+        public Person()
+        {
+            this._name = "unknown name!";
+        }
+
+        /**
+        * This is qa constructor that takes name as a parameter and passes it to
+        * the _name private instance variable
+        *
+        * @constructor Person
+        * @param {string} name
+        */
+        public Person(string name)
+        {
+            this._name = name;
+        }
     }
 }
--- COMP123-S2016-Lesson3/Program.cs
+++ COMP123-S2016-Lesson3/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lesson3
 {
+    /**
+    * This class is the "driver" class for our Program
+    *
+    * @class Program
+    */
     public class Program
     {
+        /**
+        * The main method for our driver class Program
+        *
+        * @method Main
+        * @param {string[]} args
+        */
         public static void Main(string[] args)
         {
+            // create a new instance of the Person class
+            Person person = new Person("tom");
+
+            Console.WriteLine(person.Name); // shows the constructor triggering
+
+            // set a value in the Name property of the Person class
+            person.Name = "Tom";
+
+            // get the Name value and print to console
+            Console.WriteLine(person.Name);
+
+            
         }
     }
 }
